47a8659b5ed49629c186b49d995353cc529142a0
Oleksandr <rigby8806@gmail.com>
2025-02-20 19:52:53 +0200

    correct faile  settings

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import dotenv
 
 # import dj_database_url
 from pathlib import Path
@@ -18,12 +19,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv(dotenv_path='secret.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1k_zs%uy7--clg=q(p$06y!w!p!6tjldft@ds(k#vcv-x8ptnk"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
