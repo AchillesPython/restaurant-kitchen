913445c5aeccc4aa7b59ec50c9ebdbe7d787103a
Oleksandr <rigby8806@gmail.com>
2025-02-23 18:44:30 +0200

    Solution

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "restaurant-mate-8on4.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-qhlz.onrender.com"]
 
 
 # Application definition
