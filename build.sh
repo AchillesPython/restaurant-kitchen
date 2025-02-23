a286ee5696730c038b3d8f4f6bce3e46f25003de
Oleksandr <rigby8806@gmail.com>
2025-02-23 20:10:06 +0200

    Solution

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
-!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
 
+# Встановлення залежностей
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Виконання міграцій
+python manage.py migrate --noinput
+
+# Збір статичних файлів
+python manage.py collectstatic --noinput
+
+# Створення суперкористувача (тільки якщо він не існує)
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='user').exists():
+    User.objects.create_superuser('user', 'admin@example.com', 'admin12345')
+"
+
+# Запуск Gunicorn
+gunicorn kitchen_service.wsgi:application
